bd4e373e3671503085a3b5703c885b96536ad73b
SolevieNozhki <moroz9tina@gmail.com>
2024-06-22 20:14:54 +0500

    Создание программы для определения весов товаров

--- hw1/src/Main.java
+++ hw1/src/Main.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("\t\t\t***Программа определения весов товаров***\n\n\n");
+
+        System.out.println("Введите первый вес: ");
+        double first = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите второй вес: ");
+        double second = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите третий вес: ");
+        double third = new Scanner(System.in).nextDouble();
+
+        double greatest = 0;
+        double average = 0;
+        double smallest = 0;
+
+        greatest = first >second && first>third? first: second>first && second > third?second:third;
+        smallest = first <second && first<third? first: second<first && second < third?second:third;
+
+
+
+        if((first>second && first<third) || (first<second && first >third)){
+            average = first;
+        }else if((second>first && second<third) || (second<first && second >third)){
+            average = second;
+        }else{
+            average = third;
+        }
+
+
+
+        System.out.println("Наибольший вес: " + greatest);
+        System.out.println("Средний вес: " + average);
+        System.out.println("Наименьший вес: " + smallest);
     }
 }
