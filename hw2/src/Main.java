920d6a55bcbcee6e46252c159692ad24a8f4df4e
SolevieNozhki <moroz9tina@gmail.com>
2024-06-22 20:56:39 +0500

    Создание логики программы

--- hw2/src/Main.java
+++ hw2/src/Main.java
@@ -22,6 +22,20 @@ public class Main {
         // 3. 7 слез вурдалака + 1 пельмень + 2 жабьих глаза - Эликсир скрытности
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
-
+        if(toadEyesCount >=3 && ravenBonesCount >=1){
+            System.out.println("Вы можете изготовить Эликсир зоркости");
+            isCanToCreate = true;
+        }if(ravenBonesCount >=2 && dumplingsCount >=4){
+            System.out.println("Вы можете изготовить Эликсир стойкости");
+            isCanToCreate = true;
+        }if(ghoulTearsCount >=7 && dumplingsCount >=1 && toadEyesCount >=2){
+            System.out.println("Вы можете изготовить Эликсир скрытности");
+            isCanToCreate = true;
+        }if(ghoulTearsCount >=5 && dumplingsCount >=10 && toadEyesCount >=4 && ravenBonesCount>=3) {
+            System.out.println("Вы можете изготовить Запретный эликсир");
+            isCanToCreate = true;
+        }if(isCanToCreate ==false){
+            System.out.println("К сожалению у вас не хватает ингредиентов ни на один эликсир");
+        }
     }
 }
