091762bed629a1bcf4ee253f4ee34c0dc99c2be9
SolevieNozhki <moroz9tina@gmail.com>
2024-06-23 21:31:49 +0500

    Создать массив из 10 целых чисел. Значения могут быть любыми и распечатать массив начиная с 0 до последнего индексов.

---
+++ hw3.1/src/Main.java
@@ -0,0 +1,12 @@
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] people = {10,20,15,16,17,18};
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i]);
+        }
+    }
+}
