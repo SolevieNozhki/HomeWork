574f575223b59a3e4e9c625af15ddb80c695f9d8
SolevieNozhki <moroz9tina@gmail.com>
2024-06-23 20:41:20 +0500

    Программа, которая печатает id каждого второго айтишника:Уволен айтишник с id. Через цикл while

--- hw2.1/src/Main.java
+++ hw2.1/src/Main.java
@@ -2,8 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for(int i =2;i<=100;i+=2){
+        /*for(int i =2;i<=100;i+=2){
             System.out.println("Уволен айтишник с id "+i);
+        }*/
+        int i = 2;
+        while(i<=100){
+            System.out.println("Уволен айтишник с id "+i);
+            i+=2;
         }
     }
 }
