c8b51813260f9db327dd1e1bd921f6b9955e2476
SolevieNozhki <moroz9tina@gmail.com>
2024-06-23 21:37:21 +0500

    Создать массив из 10 целых чисел. Значения могут быть любыми и hаспечатать все значения массива с конца (начиная с последнего до 0 индексов)

--- hw3.1/src/Main.java
+++ hw3.1/src/Main.java
@@ -5,7 +5,9 @@
 public class Main {
     public static void main(String[] args) {
         int[] people = {10,20,15,16,17,18};
-        for (int i = 0; i < people.length; i++) {
+        /*for (int i = 0; i < people.length; i++) {
+        }*/
+        for (int i = 5;i>=0;i--){
             System.out.println(people[i]);
         }
     }
