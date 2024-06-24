552fbd54db272c59eb403c2d8d00ff0bbf5aafeb
SolevieNozhki <moroz9tina@gmail.com>
2024-06-24 18:30:36 +0500

    Генерация веса десяти людям от 40 до 10кг. 1. Средний вес всех людей. 2. Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно.

--- hw3.1/src/Main.java
+++ hw3.1/src/Main.java
@@ -1,14 +1,20 @@
-
+import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] people = {10,20,15,16,17,18};
-        /*for (int i = 0; i < people.length; i++) {
-        }*/
-        for (int i = people.length-1;i>=0;i--){
-            System.out.println(people[i]);
+        double[] weight = new double[10];
+        double summ = 0;
+        int n =0;
+        for (int i = 0; i < weight.length; i++){
+            weight[i] = new Random().nextDouble(100-40)+40;
+            summ+=weight[i];
+            if(weight[i]>60 && weight[i]<=80){
+                n++;
+            }
         }
+        System.out.println("Средний вес всех людей:"+summ/weight.length+" кг");
+        System.out.println("Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно:"+n);
     }
 }
