60e52369023f9d209bac0fa8611305a1ffbbb22d
SolevieNozhki <moroz9tina@gmail.com>
2024-06-26 19:42:43 +0500

    Программа, в которую пользователь вводит любое целое число, а программа, если число четное, должна напечатать true. Если же число не четное, программа должна напечатать false.

--- hw4.1/src/Main.java
+++ hw4.1/src/Main.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         print();
     }
-    public static int print () {
+
+    public static void print() {
         System.out.println(getInput() % 2 == 0);
     }
-    public static int getInput () {
-            System.out.println("Введите чётное число");
-            return new Scanner(System.in).nextInt();
+
+    public static int getInput() {
+        System.out.println("Введите чётное число: ");
+        return new Scanner(System.in).nextInt();
     }
-
-
 }
